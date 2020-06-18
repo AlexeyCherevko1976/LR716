fef05adc068377c0b06aee383d94d1561e507a97
Alexey Cherevko <you@example.com>
2020-06-18 14:02:56 +0700

    k-9-9 2 dobavlenie Controllers

--- routes/api.php
+++ routes/api.php
@@ -17,32 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// k-9-7 1
+// k-9-10 2
 
-Route::get('products', function () {
-    return response(Product::all(),200);
-});
+Route::get('products', 'ProductsController@index');
  
-Route::get('products/{product}', function ($productId) {
-    return response(Product::find($productId), 200);
-});
-  
+Route::get('products/{product}', 'ProductsController@show');
  
-Route::post('products', function(Request $request) {
-   $resp = Product::create($request->all());
-    return $resp;
+Route::post('products','ProductsController@store');
  
-});
+Route::put('products/{product}','ProductsController@update');
  
-Route::put('products/{product}', function(Request $request, $productId) {
-    $product = Product::findOrFail($productId);
-    $product->update($request->all());
-    return $product;
-});
- 
-Route::delete('products/{product}',function($productId) {
-    Product::find($productId)->delete();
- 
-    return 204;
- 
-});
+Route::delete('products/{product}', 'ProductsController@delete');
