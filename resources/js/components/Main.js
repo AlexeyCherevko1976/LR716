f2fe0140509ae5c4e6f2588e39b1fe1adba0eafc
Alexey Cherevko <you@example.com>
2020-07-11 11:00:28 +0700

    1107 11-00 add clock v Main

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -43,7 +43,7 @@ class Main extends Component {
     render() { //
         return (
             <div>
-                <h3>All Products 12</h3>
+                <h3>All Products 16</h3>
                 <div>
                   { this.renderProducts() }
                 </div>
@@ -65,3 +65,16 @@ export default Main;
 if (document.getElementById('example')) {
     ReactDOM.render(<Main />, document.getElementById('example'));
 }
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
